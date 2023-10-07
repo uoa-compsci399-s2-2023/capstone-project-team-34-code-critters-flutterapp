c2e6f9c14903f11700e04ac9e94cc6c61b4109e2
JCHU634 <87676426+jchu634@users.noreply.github.com>
2023-10-08 02:57:50 +1300

    [NEW] Updated logo, name and description

--- lib/screens/preview_screen.dart
+++ lib/screens/preview_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_camera_demo/screens/captures_screen.dart';
+import 'package:CritterSleuth/screens/captures_screen.dart';
 
 class PreviewScreen extends StatelessWidget {
   final File imageFile;
