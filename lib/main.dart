fade13aa3f36294b2f18d0ac70a3289b9b7b13f9
JCHU634 <87676426+jchu634@users.noreply.github.com>
2023-10-09 02:26:41 +1300

    [FIX] Added Varela OFL to license registry

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:camera/camera.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'screens/camera_screen.dart';
 
 List<CameraDescription> cameras = [];
 
 Future<void> main() async {
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('assets/google_fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
+
   // Fetch the available cameras before initializing the app.
   try {
     WidgetsFlutterBinding.ensureInitialized();
