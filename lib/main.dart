c0c9cf9f99d94c9133038887136d56fa2dae02b3
JCHU634 <87676426+jchu634@users.noreply.github.com>
2023-10-09 01:24:26 +1300

    [FIX] Icons and Title

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'CritterSleuth',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
